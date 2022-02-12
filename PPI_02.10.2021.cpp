645f8ed63d9c0cefdd1bf1b31c09435da9855ba2
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-02-13 01:10:37 +0200

    Add files via upload
    Course: Programming Principles I
    Uploading old works to the site. 
    Works for 02/10/2021 and 04/10/2021 have been added.

---
+++ PPI_02.10.2021.cpp
@@ -0,0 +1,29 @@
+﻿/*
+		Body Mass Index(BMI) is a measure of health on weight.It can be calculated by
+	taking your weight in kilogramsand dividing by the square of your height in meters.
+	Write a program than prompts the user to enter a weight in poundsand height in
+	inchesand display the BMI.Note that 1 pound is 0.45359237 kilogram and 1 inch is
+	0.0254 meter
+
+	Made by Ivan Kosiakov on 02/10/2021
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double BMI, weight, height;
+
+	cout << "Enter weight in pounds: " << endl;
+	cin >> weight;
+
+	cout << "Enter height in inches: " << endl;
+	cin >> height;
+
+	BMI = (weight * 0.45359237 / pow(height * 0.0254, 2));
+	cout << "BMI is " << BMI << endl;
+
+	return 0;
+}
