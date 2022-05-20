e02df8c850be9bc607f2613640a50a341ef26fe1
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-05-20 12:32:29 +0300

    Add files via upload

---
+++ PPII_08.04.2022_Ex2.cpp
@@ -0,0 +1,35 @@
+/*
+		Write a void recursive function that takes an integer as parameter and prints it in
+	reverse. For example: PrintRev(234) should display 432.
+
+	Made by Ivan Kosiakov on 08/04/2022
+*/
+#include <iostream>
+
+using namespace std;
+
+void printRev(int);
+
+int main()
+{
+	int number;
+
+	cout << "Enter the number: ";
+	cin >> number;
+
+	printRev(number);
+
+	return 0;
+}
+
+void printRev(int number) {
+	int digit;
+
+	if (number < 10 && number >= 0)
+		cout << number << " ";
+	else if (number > 10) {
+		digit = number % 10;
+		cout << digit;
+		printRev(number / 10);
+	}
+}
