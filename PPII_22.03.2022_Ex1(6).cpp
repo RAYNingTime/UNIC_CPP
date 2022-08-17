5f5e91325789013f06ce74eebabe83888492aa68
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-08-17 13:39:33 +0300

    Update PPII_22.03.2022_Ex1(6).cpp
    Co-authored-by: Ivan(Janis) Ivanou <referdodanne@gmail.com>

--- PPII_22.03.2022_Ex1(6).cpp
+++ PPII_22.03.2022_Ex1(6).cpp
@@ -2,11 +2,10 @@
 		Use pointers to write a void function named remove() that deletes all occurrences of a
 	character from a C-string. The function should use two parameters: the C-string and the
 	character to be removed. For example if str contains the C-string Happy Holidays, the
-	function call remove(str, ‘H’) should change the string str to appy olidays.
+	function call remove(str, â€˜Hâ€™) should change the string str to appy olidays.
 	Test your function.
 
 		Repeat exercise 6 on Exercise Sheet 8 with functions that return a pointer to char.
-
 		Made by Ivan Kosiakov on 15/03/2022
 */
 
