329d6285eff22cee88068c5385bc0b4be7b240b6
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-12-10 13:03:06 +0200

    Create coutPractice.cpp

---
+++ PPI/Week2Lab/coutPractice.cpp
@@ -0,0 +1,20 @@
+
+// Write a C++ program that displays the following output:
+// SCORE        GRADE
+//  100           A
+//  80            B
+//  70            C
+//  60            D
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "SCORE      GRADE" << endl; 
+	cout << " 100         A" << endl;
+	cout << " 80          B" << endl;
+	cout << " 70          C" << endl;
+	cout << " 60          D" << endl;
+	return 0;
+}
